143de80a3d946ef6d8e523701e4dee69347ba8d9
Vishaltalsaniya-1 <vishal@o365.siprtc.io>
2024-12-31 15:25:31 +0530

    testc

--- cmd/model/user.go
+++
@@ -1,9 +0,0 @@
-package model
-
-// User represents a user in the system
-type User struct {
-	Id       int      `json:"id"`
-	Name     string   `json:"name"`
-	Email    string   `json:"email"`
-	Subjects []string `json:"subjects"` // Add subjects as a slice of strings
-}
--- main.go
+++ main.go
@@ -1,23 +1,53 @@
 package main
 
 import (
-	"fitness-api/cmd/db"
-	"fitness-api/cmd/router"
+	// "fitness-api/config"
+	"fitness-api/config"
+	controller "fitness-api/controller"
+	"fitness-api/db"
+	manager "fitness-api/managers"
+	"fmt"
+	"log"
 
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
-	// Initialize the database connection
-	// Replace with your PostgreSQL connection string
-	db.InitDB()
 
+	// Initialize the database
+	flagConfig, err := config.InitConfig()
+	if err != nil {
+		log.Fatalf("Error loading flag config: %v", err)
+	}
+
+	if flagConfig.FlagValue == "TRUE" {
+		// Initialize MongoDB using MongoConfig
+		fmt.Println("MongoDB URL:", flagConfig.FlagValue)
+		if err := db.InitMongoDB(); err != nil {
+			log.Fatalf("Failed to initialize MongoDB: %v", err)
+		}
+		fmt.Println("MongoDB Initialized")
+	} else {
+		// Initialize PostgreSQL
+		if err := db.InitPostgresDB(); err != nil {
+			log.Fatalf("Failed to initialize PostgreSQL: %v", err)
+		}
+		fmt.Println("PostgreSQL Initialized") 
+	}
+
+	// instanc of manager
+	userManager := manager.NewUserManager()
+	userController := controller.NewUserController(userManager)
 	// Create a new Echo instance
 	e := echo.New()
 
-	// Register routes
-	router.RegisterRoutes(e)
+	// Define routes and their handlers
+	e.POST("/users", userController.CreateUser)
+	e.GET("/users", userController.GetAllUsers)
+	e.PUT("/users/:id", userController.UpdateUser)
+	e.GET("/users/:id", userController.GetUserByID)
+	e.DELETE("/users/:id", userController.DeleteUser)
 
-	// Start the server
+	// Start the server on port 8081
 	e.Logger.Fatal(e.Start(":8081"))
 }
